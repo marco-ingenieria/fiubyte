7244fa38c3e4c9718d632432cdce26f46c8a69a5
Vilma <vgaraoz@fi.uba.ar>
2026-05-27 19:10:45 -0300

    Subo modelo base del esquema y estilo de Sitio web

--- FRONT/app.py
+++ FRONT/app.py
@@ -1,1 +1,16 @@
-#ACA VAN LOS ENDPOINTS DEL FRONTEND
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 1. Ruta del Panel Principal (Se activa al entrar a http://127.0.0.1:5000)
+@app.route('/')
+def panel_principal():
+    return render_template('panel.html')
+
+# 2. Ruta de la sección de Alumnos y Notas (Se activa al ir a /alumnos)
+@app.route('/alumnos')
+def seccion_alumnos():
+    return render_template('alumnos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
