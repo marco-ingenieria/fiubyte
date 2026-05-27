1d54ff7a43bc4349889cbceee8f859de642f0f58
amasedagustin <amaseda@fi.uba.ar>
2026-05-27 18:03:15 -0300

    dockerizar v1

---
+++ BACK/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Fiubyte API corriendo"
+
+if __name__ == "__main__":
+    #terminar de entender por que se usa host "0.0.0.0"
+    app.run(host="0.0.0.0", port=5000)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fiubyte
 Proyecto final integrador - Introducción al Desarrollo de Software - Lanzillota 1c2026 - Grupo 19 BACKLOVERS
+
+# completen el .env y ejecuten el comando para arrancar docker
+docker compose up --build
