8b6b2b17feeb9986185cfa93ebef1656a2bbc750
Hexagoñ-6 <marcojleanza5@gmail.com>
2026-06-02 22:08:40 -0300

    creado endpoints de historiales

--- BACK/app.py
+++ BACK/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from routes.alumnos import alumnos_bp
 from routes.usuarios import usuarios_bp
 from routes.evaluaciones import evaluaciones_bp
+from routes.historial import historiales_bp
 app = Flask(__name__)
 
 app.register_blueprint(alumnos_bp, url_prefix='/alumnos')
 app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
 app.register_blueprint(evaluaciones_bp, url_prefix='/evaluaciones')
+app.register_blueprint(historiales_bp, url_prefix='/historiales')
 
 @app.route("/")
 def index():
