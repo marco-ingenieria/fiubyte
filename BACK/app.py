db5d095f5efc6416fcced8c20352001b0b3de333
amasedagustin <amaseda@fi.uba.ar>
2026-06-03 21:14:22 -0300

    API Notas Filtros

--- BACK/app.py
+++ BACK/app.py
@@ -3,17 +3,18 @@ from routes.alumnos import alumnos_bp
 from routes.usuarios import usuarios_bp
 from routes.evaluaciones import evaluaciones_bp
 from routes.historial import historiales_bp
+from routes.notas import notas_bp
 app = Flask(__name__)
 
 app.register_blueprint(alumnos_bp, url_prefix='/alumnos')
 app.register_blueprint(usuarios_bp, url_prefix='/usuarios')
 app.register_blueprint(evaluaciones_bp, url_prefix='/evaluaciones')
 app.register_blueprint(historiales_bp, url_prefix='/historiales')
+app.register_blueprint(notas_bp, url_prefix='/notas')
 
 @app.route("/")
 def index():
     return "Fiubyte API corriendo"
 
 if __name__ == "__main__":
-    #terminar de entender por que se usa host "0.0.0.0"
     app.run(host="0.0.0.0", port=5000)
